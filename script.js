9695ad47530b0ee6c4782bafd288a0cde5801787
Brandon Shankar <bshankar12@comcast.net>
2020-12-13 21:37:22 -0800

    added js for computing

--- script.js
+++ script.js
@@ -24,12 +24,39 @@ class calculator {
     }
 
     chooseOperation(operation) {
+        if (this.currentOperand === '') return;
+        if (this.previousOperand !=='')   {
+            this.compute();
+        }
         this.operation = operation;
         this.previousOperand = this.currentOperand;
         this.currentOperand = '';
 
     }
     compute() {
+        var computation;
+        var prev = parseFloat(this.previousOperand);
+        var current = parseFloat(this.currentOperand);
+        if (isNaN(prev) || isNaN(current)) return;
+        switch (this.operation) {
+            case '+': 
+                computation = prev + current;
+                break;
+            case '-': 
+                computation = prev - current;
+                break;
+            case '*': 
+                computation = prev * current;
+                break;
+            case '÷': 
+                computation = prev / current;
+                break;
+            default: 
+            return;
+        }
+        this.currentOperand = computation;
+        this.operation = undefined;
+        this.previousOperand = '';
 
     }
 
@@ -67,4 +94,15 @@ operationButtons.forEach(button => {
         calculator.updateDisplay()
 
     })
+})
+
+
+equalsButton.addEventListener('click', button =>   {
+    calculator.compute();
+    calculator.updateDisplay();
+})
+
+allClearButton.addEventListener('click', button =>   {
+    calculator.compute();
+    calculator.updateDisplay();
 })
